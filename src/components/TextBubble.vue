9be15b279be927726c8990a67a31cb4d027ca4bf
LucaCode <luuk.goedhart@soprasteria.com>
2025-06-18 13:06:26 +0200

    Adds textbubble and sopra logo

---
+++ src/components/TextBubble.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps<{text: string}>();
+
+</script><template>
+    <div>
+        {{ props.text }}
+    </div>
+</template>
+<style scoped>
+    div {
+        border-radius: 100%;
+        aspect-ratio: 1/1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        /* width: 100%; */
+        height: 150px;
+        width: 150px;
+        /* padding: px; */
+        background-color: black;
+        text-wrap: wrap;
+        font-size: 1.5em;
+        border: 3px solid var(--highlight-color);
+    }
+
+</style>
